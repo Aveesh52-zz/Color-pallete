b9d5e9cadb579ac012d94a470d1c9b54639298e6
Aveesh51 <aveeshshetty120@gmail.com>
2019-08-17 13:35:10 +0530

    Color-pallete

---
+++ build/precache-manifest.e0f20473a9638f378002e94fa4c3920b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ab73e7b1d07369ebd00",
+    "url": "/static/js/main.ff4ad4bf.chunk.js"
+  },
+  {
+    "revision": "7f4a4f49dc0d64d3d2c7",
+    "url": "/static/js/2.c439ba46.chunk.js"
+  },
+  {
+    "revision": "6ab73e7b1d07369ebd00",
+    "url": "/static/css/main.63c9ab37.chunk.css"
+  },
+  {
+    "revision": "7f4a4f49dc0d64d3d2c7",
+    "url": "/static/css/2.e2d49c86.chunk.css"
+  },
+  {
+    "revision": "235b20ff4ea739c6c9c6aa80de30c11f",
+    "url": "/index.html"
+  }
+];
